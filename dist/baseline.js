480290b47a588f4af7471c276fdd802c3e20a78a
Bhushan Patil <bhushan.online@yahoo.com>
2019-11-07 01:20:06 +0530

    Add CSS outlines in an accessible manner using JS

--- dist/baseline.js
+++ dist/baseline.js
@@ -1,1 +1,21 @@
 "use strict";
+
+var baseline = {
+  removFocusOutline: function removFocusOutline() {
+    // Removes CSS outlines in an accessible manner.
+    var styleElement = document.createElement('style');
+    document.getElementsByTagName('head')[0].appendChild(styleElement); // Using mousedown instead of mouseover, so that previously focused elements don't lose focus ring on mouse move
+
+    document.addEventListener('mousedown', function () {
+      styleElement.innerHTML = ':focus{outline:none!important}';
+    });
+    document.addEventListener('keydown', function (event) {
+      if (9 === event.keyCode) {
+        styleElement.innerHTML = '';
+      }
+    });
+  }
+};
+document.addEventListener('DOMContentLoaded', function () {
+  baseline.removFocusOutline();
+}); // window.addEventListener( 'load', function() {});
---
+++ src/js/baseline.js
@@ -0,0 +1,27 @@
+const baseline = {
+	removFocusOutline: function() {
+
+		// Removes CSS outlines in an accessible manner.
+
+		const styleElement = document.createElement( 'style' );
+
+		document.getElementsByTagName( 'head' )[0].appendChild( styleElement );
+
+		// Using mousedown instead of mouseover, so that previously focused elements don't lose focus ring on mouse move
+		document.addEventListener( 'mousedown', function() {
+			styleElement.innerHTML = ':focus{outline:none!important}';
+		});
+
+		document.addEventListener( 'keydown', function( event ) {
+			if ( 9 === event.keyCode ) {
+				styleElement.innerHTML = '';
+			}
+		});
+	},
+};
+
+document.addEventListener( 'DOMContentLoaded', function() {
+	baseline.removFocusOutline();
+});
+
+// window.addEventListener( 'load', function() {});

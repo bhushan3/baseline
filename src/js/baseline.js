61aa80378ccf0bde04c8b753549aef33d2d4d7da
Bhushan Patil <bhushan.online@yahoo.com>
2020-02-18 23:48:08 +0530

    Added components

--- src/js/baseline.js
+++ src/js/baseline.js
@@ -18,10 +18,73 @@ const baseline = {
 			}
 		});
 	},
+	components: function() {
+		document.querySelectorAll( '.textfield' ).forEach( function( textfield ) {
+			textfield.querySelector( '.textfield__input' ).addEventListener( 'focusout', function( event ) {
+				if ( event.target.value ) {
+					textfield.classList.add( 'textfield--with-value' );
+				} else {
+					textfield.classList.remove( 'textfield--with-value' );
+				}
+			});
+			if ( textfield.querySelector( '.textfield__icon--password-visibility-toggle' ) ) {
+				textfield.querySelector( '.textfield__icon--password-visibility-toggle' ).addEventListener( 'click', function( event ) {
+					if ( 'password' === textfield.querySelector( '.textfield__input' ).type ) {
+						textfield.querySelector( '.textfield__input' ).type = 'text';
+						event.target.innerHTML = 'visibility';
+					} else {
+						textfield.querySelector( '.textfield__input' ).type = 'password';
+						event.target.innerHTML = 'visibility_off';
+					}
+				});
+			}
+		});
+		document.querySelectorAll( '.textfield-filled' ).forEach( function( textfield ) {
+			textfield.querySelector( '.textfield-filled__input' ).addEventListener( 'focusout', function( event ) {
+				if ( event.target.value ) {
+					textfield.classList.add( 'textfield-filled--with-value' );
+				} else {
+					textfield.classList.remove( 'textfield-filled--with-value' );
+				}
+			});
+			if ( textfield.querySelector( '.textfield-filled__icon--password-visibility-toggle' ) ) {
+				textfield.querySelector( '.textfield-filled__icon--password-visibility-toggle' ).addEventListener( 'click', function( event ) {
+					if ( 'password' === textfield.querySelector( '.textfield-filled__input' ).type ) {
+						textfield.querySelector( '.textfield-filled__input' ).type = 'text';
+						event.target.innerHTML = 'visibility';
+					} else {
+						textfield.querySelector( '.textfield-filled__input' ).type = 'password';
+						event.target.innerHTML = 'visibility_off';
+					}
+				});
+			}
+		});
+		document.querySelectorAll( '.textfield-outlined' ).forEach( function( textfield ) {
+			textfield.querySelector( '.textfield-outlined__input' ).addEventListener( 'focusout', function( event ) {
+				if ( event.target.value ) {
+					textfield.classList.add( 'textfield-outlined--with-value' );
+				} else {
+					textfield.classList.remove( 'textfield-outlined--with-value' );
+				}
+			});
+			if ( textfield.querySelector( '.textfield-outlined__icon--password-visibility-toggle' ) ) {
+				textfield.querySelector( '.textfield-outlined__icon--password-visibility-toggle' ).addEventListener( 'click', function( event ) {
+					if ( 'password' === textfield.querySelector( '.textfield-outlined__input' ).type ) {
+						textfield.querySelector( '.textfield-outlined__input' ).type = 'text';
+						event.target.innerHTML = 'visibility';
+					} else {
+						textfield.querySelector( '.textfield-outlined__input' ).type = 'password';
+						event.target.innerHTML = 'visibility_off';
+					}
+				});
+			}
+		});
+	},
 };
 
 document.addEventListener( 'DOMContentLoaded', function() {
 	baseline.removFocusOutline();
+	baseline.components();
 });
 
 // window.addEventListener( 'load', function() {});
